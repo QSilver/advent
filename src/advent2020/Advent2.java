91feeba1ac41e1db535b66d232512356f25d956e
QSilver <geo_007.popa@yahoo.com>
2020-12-02 10:58:25 +0000

    2020 Day 2

--- src/advent2020/Advent2.java
+++ src/advent2020/Advent2.java
@@ -5,42 +5,46 @@ import util.Util;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Slf4j
 public class Advent2 {
     public static void main(String[] args) {
-        solvePart2();
+        solve(checkCount());
+        solve(checkPositions());
     }
 
-    private static void solvePart1() {
+    private static void solve(Predicate<String[]> function) {
         long nrOfPasswords = Util.fileStream("advent2020/advent2")
                                  .map(s -> s.split(" "))
-                                 .filter(strings -> {
-                                     List<Integer> bounds = Arrays.stream(strings[0].split("-"))
-                                                                  .map(Integer::parseInt)
-                                                                  .collect(Collectors.toList());
-                                     char letter = strings[1].split(":")[0].charAt(0);
-                                     long count = strings[2].chars()
-                                                            .filter(ch -> ch == letter)
-                                                            .count();
-                                     return bounds.get(0) <= count && count <= bounds.get(1);
-                                 })
+                                 .filter(function)
                                  .count();
         log.info("Nr of passwords: {}", nrOfPasswords);
     }
 
-    private static void solvePart2() {
-        long nrOfPasswords = Util.fileStream("advent2020/advent2")
-                                 .map(s -> s.split(" "))
-                                 .filter(strings -> {
-                                     List<Integer> positions = Arrays.stream(strings[0].split("-"))
-                                                                     .map(Integer::parseInt)
-                                                                     .collect(Collectors.toList());
-                                     char letter = strings[1].split(":")[0].charAt(0);
-                                     return strings[2].charAt(positions.get(0) - 1) == letter ^ strings[2].charAt(positions.get(1) - 1) == letter;
-                                 })
-                                 .count();
-        log.info("Nr of passwords: {}", nrOfPasswords);
+    private static Predicate<String[]> checkCount() {
+        return strings -> {
+            List<Integer> bounds = getIntegers(strings[0]);
+            char letter = strings[1].split(":")[0].charAt(0);
+            long count = strings[2].chars()
+                                   .filter(ch -> ch == letter)
+                                   .count();
+            return bounds.get(0) <= count && count <= bounds.get(1);
+        };
+    }
+
+    private static Predicate<String[]> checkPositions() {
+        return strings -> {
+            List<Integer> positions = getIntegers(strings[0]);
+            char letter = strings[1].split(":")[0].charAt(0);
+            return strings[2].charAt(positions.get(0) - 1) == letter ^ strings[2].charAt(positions.get(1) - 1) == letter;
+        };
+    }
+
+    private static List<Integer> getIntegers(String string) {
+        return Arrays.stream(string.split("-"))
+                     .map(Integer::parseInt)
+                     .collect(Collectors.toList());
     }
 }
