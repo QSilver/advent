5f19fda291b1d836c40c713869cdd6ad154d0133
QSilver <geo_007.popa@yahoo.com>
2021-12-07 13:33:23 +0000

    2021 Day 7

--- src/advent2021/Advent7.java
+++ src/advent2021/Advent7.java
@@ -4,44 +4,47 @@ import lombok.extern.slf4j.Slf4j;
 import util.Util;
 
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Advent7 {
     public static void main(String[] args) {
-        Stream<Integer> crabs = Util.fileStream("advent2021/advent7")
-                                    .map(s -> s.split(","))
-                                    .flatMap(Arrays::stream)
-                                    .map(Integer::parseInt);
+        List<Integer> crabs = Util.fileStream("advent2021/advent7")
+                                  .map(s -> s.split(","))
+                                  .flatMap(Arrays::stream)
+                                  .map(Integer::parseInt)
+                                  .collect(Collectors.toList());
 
-        double median = crabs.sorted()
-                             .skip(Math.max(0, ((crabs.count() + 1) / 2) - 1))
-                             .limit(1 + (1 + crabs.count()) % 2)
+        double median = crabs.stream()
+                             .sorted()
+                             .skip(Math.max(0, (((long) crabs.size() + 1) >> 1) - 1))
+                             .limit(1 + (1 + (long) crabs.size()) % 2)
                              .mapToInt(Integer::intValue)
                              .average()
                              .getAsDouble();
         log.info("median: {}", median);
 
-        int fuel_linear = crabs
-                .map(integer -> Math.abs(integer - (int) median))
-                .mapToInt(value -> value)
-                .sum();
+        int fuel_linear = crabs.stream()
+                               .map(integer -> Math.abs(integer - (int) median))
+                               .mapToInt(value -> value)
+                               .sum();
         log.info("P1: {}", fuel_linear);
 
-        double mean = crabs
-                .mapToInt(Integer::intValue)
-                .average()
-                .getAsDouble();
+        double mean = crabs.stream()
+                           .mapToInt(Integer::intValue)
+                           .average()
+                           .getAsDouble();
         log.info("mean: {}", mean);
 
-        int fuel_gauss = crabs
-                .map(integer -> {
-                    int a = Math.abs(integer - (int) mean);
-                    return a * (a + 1) / 2;
-                })
-                .sorted()
-                .mapToInt(value -> value)
-                .sum();
+        int fuel_gauss = crabs.stream()
+                              .map(integer -> {
+                                  int a = Math.abs(integer - (int) mean);
+                                  return a * (a + 1) >> 1;
+                              })
+                              .sorted()
+                              .mapToInt(value -> value)
+                              .sum();
         log.info("P2: {}", fuel_gauss);
     }
 }
