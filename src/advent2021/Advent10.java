57f1819a2b2aecc10247711053a383d510171d7f
QSilver <geo_007.popa@yahoo.com>
2021-12-10 11:10:40 +0000

    2021 Day 10

--- src/advent2021/Advent10.java
+++ src/advent2021/Advent10.java
@@ -5,103 +5,51 @@ import util.Util;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Stack;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Maps.newHashMap;
-import static com.google.common.collect.Sets.newHashSet;
 
 @Slf4j
 public class Advent10 {
 
-    static Set<Character> open = newHashSet('(', '[', '{', '<');
-    static Map<Character, Character> complement = newHashMap();
+    static Map<Character, Character> complement = Map.of('(', ')', '[', ']', '{', '}', '<', '>');
+    static Map<Character, Integer> corruptedScores = Map.of(')', 3, ']', 57, '}', 1197, '>', 25137);
+    static Map<Character, Integer> endingScores = Map.of(')', 1, ']', 2, '}', 3, '>', 4);
 
     public static void main(String[] args) {
-        complement.put('(', ')');
-        complement.put('[', ']');
-        complement.put('{', '}');
-        complement.put('<', '>');
+        AtomicInteger points = new AtomicInteger();
+        List<Long> scores = newArrayList();
 
-        List<Character> corrupted = newArrayList();
-        List<String> endings = newArrayList();
         Util.fileStream("advent2021/advent10")
             .forEach(line -> {
                 boolean isCorrupted = false;
                 Stack<Character> charStack = new Stack<>();
                 for (char c : line.toCharArray()) {
-                    if (open.contains(c)) {
+                    if (complement.containsKey(c)) {
                         charStack.push(c);
                     } else if (complement.get(charStack.peek()) == c) {
                         charStack.pop();
                     } else {
-                        corrupted.add(c);
+                        points.addAndGet(corruptedScores.get(c));
                         isCorrupted = true;
                         break;
                     }
                 }
                 if (!isCorrupted) {
-                    StringBuilder ending = new StringBuilder();
-                    charStack.forEach(c -> ending.append(complement.get(c)));
-                    endings.add(ending.reverse()
-                                      .toString());
+                    long tempScore = 0;
+                    while (!charStack.empty()) {
+                        tempScore = tempScore * 5 + endingScores.get(complement.get(charStack.pop()));
+                    }
+                    scores.add(tempScore);
                 }
             });
 
-        int points = calculateCorruptedScores(corrupted);
         log.info("P1: {}", points);
-
-        List<Long> scores = calculateEndingScores(endings);
-        log.info("Endings: {}", endings);
         log.info("P2: {}", scores.stream()
                                  .sorted()
                                  .collect(Collectors.toList())
                                  .get(scores.size() / 2));
-    }
-
-    private static int calculateCorruptedScores(List<Character> corrupted) {
-        return corrupted.stream()
-                        .map(c -> {
-                            if (c == ')') {
-                                return 3;
-                            }
-                            if (c == ']') {
-                                return 57;
-                            }
-                            if (c == '}') {
-                                return 1197;
-                            }
-                            if (c == '>') {
-                                return 25137;
-                            }
-                            return 0;
-                        })
-                        .mapToInt(value -> value)
-                        .sum();
-    }
-
-    private static List<Long> calculateEndingScores(List<String> endings) {
-        return endings.stream()
-                      .map(end -> {
-                          AtomicLong tempScore = new AtomicLong();
-                          end.chars()
-                             .forEach(value -> {
-                                 tempScore.updateAndGet(v -> v * 5);
-                                 if (value == ')') {
-                                     tempScore.getAndAdd(1);
-                                 } else if (value == ']') {
-                                     tempScore.getAndAdd(2);
-                                 } else if (value == '}') {
-                                     tempScore.getAndAdd(3);
-                                 } else if (value == '>') {
-                                     tempScore.getAndAdd(4);
-                                 }
-                             });
-                          return tempScore.get();
-                      })
-                      .collect(Collectors.toList());
     }
 }
