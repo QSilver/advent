12cce4b5db373be9e21ae49447365394eb1b7576
QSilver <geo_007.popa@yahoo.com>
2022-12-03 10:01:09 +0000

    2022 Day 2-3

---
+++ src/advent2022/Advent2.java
@@ -0,0 +1,45 @@
+package advent2022;
+
+import com.google.common.collect.ImmutableMap;
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+// 12701 - too high
+@Slf4j
+public class Advent2 {
+    static final Map<String, Integer> scoreMap = ImmutableMap.<String, Integer>builder()
+            .put("A X", 1 + 3)
+            .put("A Y", 2 + 6)
+            .put("A Z", 3 + 0)
+            .put("B X", 1 + 0)
+            .put("B Y", 2 + 3)
+            .put("B Z", 3 + 6)
+            .put("C X", 1 + 6)
+            .put("C Y", 2 + 0)
+            .put("C Z", 3 + 3)
+            .build();
+
+    static final Map<String, Integer> modifiedScoreMap = ImmutableMap.<String, Integer>builder()
+            .put("A X", 3 + 0)
+            .put("A Y", 1 + 3)
+            .put("A Z", 2 + 6)
+            .put("B X", 1 + 0)
+            .put("B Y", 2 + 3)
+            .put("B Z", 3 + 6)
+            .put("C X", 2 + 0)
+            .put("C Y", 3 + 3)
+            .put("C Z", 1 + 6)
+            .build();
+
+    public static void main(String[] args) {
+        List<String> input = Util.fileStream("advent2022/advent2").collect(Collectors.toList());
+        int sum = input.stream().map(scoreMap::get).mapToInt(value -> value).sum();
+        int modifiedSum = input.stream().map(modifiedScoreMap::get).mapToInt(value -> value).sum();
+        log.info("Sum: {}", sum);
+        log.info("ModifiedSum: {}", modifiedSum);
+    }
+}
---
+++ src/advent2022/Advent3.java
@@ -0,0 +1,73 @@
+package advent2022;
+
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// 12701 - too high
+@Slf4j
+public class Advent3 {
+    public static void main(String[] args) {
+        List<String> input = Util.fileStream("advent2022/advent3").collect(Collectors.toList());
+
+        int sum = part1(input);
+        log.info("Item Sum: {}", sum);
+
+        int sum2 = part2(input);
+        log.info("Badge Sum: {}", sum2);
+    }
+
+    private static int part2(List<String> input) {
+        int score = 0;
+        for (int g = 0; g < input.size(); g += 3) {
+            int[] firstFreq = countFrequencies(input.get(g));
+            int[] secondFreq = countFrequencies(input.get(g + 1));
+            int[] thirdFreq = countFrequencies(input.get(g + 2));
+
+
+            for (int i = 0; i < firstFreq.length; i++) {
+                if (firstFreq[i] > 0 && secondFreq[i] > 0 && thirdFreq[i] > 0) {
+                    score += getPriority(i);
+                }
+            }
+        }
+        return score;
+    }
+
+    private static int part1(List<String> input) {
+        return input.stream().mapToInt(s -> {
+            String first = s.substring(0, s.length() / 2);
+            String second = s.substring(s.length() / 2);
+
+            int[] firstFreq = countFrequencies(first);
+            int[] secondFreq = countFrequencies(second);
+
+            int score = 0;
+            for (int i = 0; i < firstFreq.length; i++) {
+                if (firstFreq[i] > 0 && secondFreq[i] > 0) {
+                    score += getPriority(i);
+                }
+            }
+
+            return score;
+        }).sum();
+    }
+
+    private static int[] countFrequencies(String input) {
+        int[] firstFreq = new int[58];
+        for (int i = 0; i < input.length(); i++) {
+            firstFreq[input.charAt(i) - 65]++;
+        }
+        return firstFreq;
+    }
+
+    private static int getPriority(int i) {
+        if (i <= 26) {
+            return 27 + i;
+        } else {
+            return i - 31;
+        }
+    }
+}
