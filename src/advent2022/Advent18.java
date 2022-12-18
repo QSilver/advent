d113dd37e12c37594676dd49c67a8e27fca6f99c
QSilver <geo_007.popa@yahoo.com>
2022-12-18 10:23:08 +0000

    2022 Day 18 - part 2

--- src/advent2022/Advent18.java
+++ src/advent2022/Advent18.java
@@ -6,8 +6,8 @@ import util.Util;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
@@ -17,26 +17,19 @@ import static com.google.common.collect.Queues.newArrayDeque;
 @Slf4j
 public class Advent18 {
     static int exteriorSides;
-    static boolean[][][] cubes;
-    static int maxX;
-    static int maxY;
-    static int maxZ;
 
     public static void main(String[] args) {
-        Set<Cube> cubeSet = Util.fileStream("advent2022/advent18")
+        Map<Point3D, Cube> cubeMap = Util.fileStream("advent2022/advent18")
                 .map(Cube::new)
-                .collect(Collectors.toSet());
-        maxX = cubeSet.stream().max(Comparator.comparingInt(cube -> cube.coords.x)).get().coords.x;
-        maxY = cubeSet.stream().max(Comparator.comparingInt(cube -> cube.coords.y)).get().coords.y;
-        maxZ = cubeSet.stream().max(Comparator.comparingInt(cube -> cube.coords.z)).get().coords.z;
-        cubes = new boolean[maxX + 2][maxY + 2][maxZ + 2];
-        cubeSet.forEach(cube -> cubes[cube.coords.x][cube.coords.y][cube.coords.z] = true);
-
-        part1(cubeSet);
-        part2(cubeSet);
+                .collect(Collectors.toMap(cube -> cube.coords, cube -> cube));
+        part1(cubeMap);
+        part2(cubeMap);
     }
 
-    private static void part2(Set<Cube> cubeSet) {
+    private static void part2(Map<Point3D, Cube> cubeMap) {
+        int maxX = cubeMap.keySet().stream().max(Comparator.comparingInt(point3D -> point3D.x)).get().x;
+        int maxY = cubeMap.keySet().stream().max(Comparator.comparingInt(point3D -> point3D.y)).get().y;
+        int maxZ = cubeMap.keySet().stream().max(Comparator.comparingInt(point3D -> point3D.z)).get().z;
         boolean[][][] visited = new boolean[maxX + 2][maxY + 2][maxZ + 2];
         Queue<Point3D> floodPoints = newArrayDeque();
         floodPoints.add(new Point3D(0, 0, 0));
@@ -44,14 +37,14 @@ public class Advent18 {
         while (floodPoints.size() > 0) {
             Queue<Point3D> nextPoints = newArrayDeque();
             for (Point3D source : floodPoints) {
-                nextPoints.addAll(flood(source, visited, cubeSet));
+                nextPoints.addAll(flood(source, visited, cubeMap));
             }
             floodPoints = nextPoints;
         }
         log.info("Exterior Surface: {}", exteriorSides);
     }
 
-    private static List<Point3D> flood(Point3D source, boolean[][][] visited, Set<Cube> cubeSet) {
+    private static List<Point3D> flood(Point3D source, boolean[][][] visited, Map<Point3D, Cube> cubeMap) {
         List<Point3D> possible = newArrayList();
         possible.add(new Point3D(source.x - 1, source.y, source.z));
         possible.add(new Point3D(source.x + 1, source.y, source.z));
@@ -65,14 +58,14 @@ public class Advent18 {
             // if next point is in bounds
             if (nextPoint.x >= 0 && nextPoint.y >= 0 && nextPoint.z >= 0 && nextPoint.x < visited.length && nextPoint.y < visited[0].length && nextPoint.z < visited[0][0].length) {
                 // count touching cubes
-                if (cubes[nextPoint.x][nextPoint.y][nextPoint.z]) {
+                if (cubeMap.containsKey(nextPoint)) {
                     exteriorSides++;
                 }
 
                 // if next point is not already visited
                 if (!visited[nextPoint.x][nextPoint.y][nextPoint.z]) {
                     // if next point not a cube
-                    if (!cubes[nextPoint.x][nextPoint.y][nextPoint.z]) {
+                    if (!cubeMap.containsKey(nextPoint)) {
                         nextPoints.add(nextPoint);
                         visited[nextPoint.x][nextPoint.y][nextPoint.z] = true;
                     }
@@ -82,10 +75,11 @@ public class Advent18 {
         return nextPoints;
     }
 
-    private static void part1(Set<Cube> cubeSet) {
+    private static void part1(Map<Point3D, Cube> cubeMap) {
         List<Point3D> openSides = newArrayList();
-        cubeSet.forEach(cube -> cube.sides.forEach(side -> {
-            if (!cubes[side.x][side.y][side.z]) {
+        cubeMap.forEach((key, value) -> value.sides.forEach(side -> {
+            // if no cube exists where side would be
+            if (!cubeMap.containsKey(side)) {
                 openSides.add(side);
             }
         }));
