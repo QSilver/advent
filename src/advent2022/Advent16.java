3de0d417e564e818a74c3d00dbbc3549778549ec
QSilver <geo_007.popa@yahoo.com>
2023-01-22 16:11:35 +0000

    2022 Day 16 rewrite + part 2

--- src/advent2022/Advent16.java
+++ src/advent2022/Advent16.java
@@ -6,15 +6,17 @@ import org.apache.commons.math3.util.Pair;
 import util.Util;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
+import static com.google.common.collect.Sets.newHashSet;
 
-// 1725 - too low
 @Slf4j
 public class Advent16 {
     public static final String START = "AA";
@@ -23,38 +25,83 @@ public class Advent16 {
 
     static Map<Pair<String, String>, Integer> distances = newHashMap();
 
+    static Map<Set<String>, Integer> releasePressureForValveCombination = newHashMap();
+
     public static void main(String[] args) {
         rooms = Util.fileStream("advent2022/advent16")
                 .map(Room::new).collect(Collectors.toMap(room -> room.label, room -> room));
 
         floydWarshall();
 
-        Path starting = new Path();
-        starting.route.add(START);
-        starting.turn = 1;
+        log.info("Best Path: {}", explore(rooms.get(START), 30, newArrayList(), 0));
 
-        List<Path> paths = newArrayList(starting);
-        while (true) {
-            List<Path> newPaths = paths.stream()
-                    .map(Path::generatePaths)
-                    .flatMap(Collection::stream)
-                    .collect(Collectors.toList());
-            log.info("New Iteration Size: {}", newPaths.size());
-            if (newPaths.size() == 0) {
-                break;
+        releasePressureForValveCombination.clear();
+        explore(rooms.get(START), 26, newArrayList(), 0);
+
+        AtomicInteger maxElephantPressure = new AtomicInteger();
+        AtomicReference<Pair<Set<String>, Set<String>>> savedPath = new AtomicReference<>();
+
+        releasePressureForValveCombination.forEach((strings1, integer1) -> {
+            releasePressureForValveCombination.forEach((strings2, integer2) -> {
+                Set<String> intersection = newHashSet(strings1);
+                intersection.retainAll(strings2);
+
+                if (intersection.size() == 0) {
+                    if (maxElephantPressure.get() < integer1 + integer2) {
+                        maxElephantPressure.set(integer1 + integer2);
+                        savedPath.set(Pair.create(strings1, strings2));
+                    }
+                }
+            });
+        });
+        log.info("Best Path with Elephant: {} - Path 1: {} - Path 2: {}", maxElephantPressure.get(), savedPath.get().getFirst(), savedPath.get().getSecond());
+    }
+
+    private static Pair<Integer, List<String>> explore(Room current, int timeRemaining, List<String> openValves, int releasedWhenMoving) {
+        int thisPressure = openValves
+                .stream()
+                .map(valve -> rooms.get(valve).flowRate)
+                .mapToInt(Integer::intValue)
+                .sum();
+        int maxPressure = thisPressure * timeRemaining;
+
+        Set<String> currentValves = newHashSet(openValves);
+        if (releasePressureForValveCombination.containsKey(currentValves)) {
+            if (releasePressureForValveCombination.get(currentValves) < maxPressure + releasedWhenMoving) {
+                releasePressureForValveCombination.put(currentValves, maxPressure + releasedWhenMoving);
             }
-            paths = newPaths;
+        } else {
+            releasePressureForValveCombination.put(currentValves, maxPressure + releasedWhenMoving);
         }
 
-        paths.forEach(path -> {
-            if (path.turn < 31) {
-                path.pressure += (path.opened.stream().mapToInt(s -> rooms.get(s).flowRate * (31 - path.turn))).sum();
-                path.turn = 31;
-            }
-        });
+        List<String> bestRoute = newArrayList();
 
-        List<Path> sortedPaths = paths.stream().sorted((o1, o2) -> Integer.compare(o2.pressure, o1.pressure)).collect(Collectors.toList());
-        log.info("Max Pressure: {}", sortedPaths.get(0).pressure);
+        List<Room> valves = rooms.values().stream()
+                .filter(room -> room.flowRate > 0)
+                .filter(room -> !room.label.equals(current.label))
+                .collect(Collectors.toList());
+
+        for (Room valve : valves) {
+            int movesToOpen = distances.get(Pair.create(current.label, valve.label)) + 1;
+            if (timeRemaining >= movesToOpen && !openValves.contains(valve.label)) {
+                openValves.add(valve.label);
+                releasedWhenMoving += thisPressure * movesToOpen;
+                Pair<Integer, List<String>> explore = explore(rooms.get(valve.label), timeRemaining - movesToOpen, openValves, releasedWhenMoving);
+                int subPressure = explore.getFirst() + thisPressure * movesToOpen;
+
+                if (subPressure > maxPressure) {
+                    maxPressure = subPressure;
+                    bestRoute.clear();
+                    bestRoute.add(valve.label);
+                    bestRoute.addAll(explore.getSecond());
+                }
+
+                releasedWhenMoving -= thisPressure * movesToOpen;
+                openValves.remove(openValves.size() - 1);
+            }
+        }
+
+        return Pair.create(maxPressure, bestRoute);
     }
 
     private static void floydWarshall() {
@@ -77,54 +124,6 @@ public class Advent16 {
                 .filter(entry -> rooms.get(entry.getKey().getFirst()).flowRate > 0 || START.equals(entry.getKey().getFirst()))
                 .filter(entry -> rooms.get(entry.getKey().getSecond()).flowRate > 0)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    static class Path {
-        List<String> route = newArrayList();
-        List<String> opened = newArrayList();
-        int pressure;
-        int turn;
-
-        List<Path> generatePaths() {
-            openValve();
-
-            List<Path> possiblePaths = newArrayList();
-            Room current = rooms.get(route.get(route.size() - 1));
-            distances.entrySet().stream()
-                    .filter(entry -> current.label.equals(entry.getKey().getFirst())) // all connections
-                    .filter(entry -> !route.contains(entry.getKey().getSecond())) // unvisited
-                    .forEach(entry -> {
-                        if (turn + entry.getValue() < 31) { // can reach node
-                            possiblePaths.add(createNewPath(entry));
-                        }
-                    });
-            return possiblePaths;
-        }
-
-        private Path createNewPath(Map.Entry<Pair<String, String>, Integer> entry) {
-            Path newPath = new Path();
-            newPath.route = newArrayList(route);
-            newPath.route.add(entry.getKey().getSecond());
-            newPath.opened = newArrayList(opened);
-            newPath.pressure = pressure;
-            newPath.pressure += (newPath.opened.stream().mapToInt(s -> rooms.get(s).flowRate * entry.getValue())).sum();
-            newPath.turn = turn + entry.getValue();
-            return newPath;
-        }
-
-        void openValve() {
-            Room current = rooms.get(route.get(route.size() - 1));
-            if (!opened.contains(current.label) && current.flowRate > 0) {
-                turn++;
-                pressure += (opened.stream().mapToInt(s -> rooms.get(s).flowRate)).sum();
-                opened.add(current.label);
-            }
-        }
-
-        @Override
-        public String toString() {
-            return route + ", " + opened + ", pR=" + pressure + ", min=" + turn;
-        }
     }
 
     static class Room {
