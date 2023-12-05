85a55ea7232e4f1d89a184a823499f44af265983
QSilver <geo_007.popa@yahoo.com>
2023-12-05 20:26:43 +0000

    2023 Day 5 - cleanup

--- src/advent2023/Advent5.java
+++ src/advent2023/Advent5.java
@@ -4,14 +4,17 @@ import lombok.With;
 import lombok.extern.slf4j.Slf4j;
 import util.Util;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static java.lang.Long.min;
 import static java.lang.Long.parseLong;
+import static java.util.Comparator.comparing;
 
 // 3244927 - too low
 
@@ -22,21 +25,9 @@ public class Advent5 {
     List<String> types = newArrayList("seed", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location");
 
     public Long runP1(String file) {
-        String[] split = Util.fileStream(file)
-                .collect(Collectors.joining("\n"))
-                .split("\n\n");
-
-        types.forEach(s -> translations.put(s, newArrayList()));
-
-        for (int i = 1; i < split.length; i++) {
-            String[] lines = split[i].split("\n");
-            String mapType = lines[0].split(" ")[0].split("-")[2];
-            translations.get(mapType).addAll(Arrays.asList(lines).subList(1, lines.length));
-        }
+        String[] seeds = parseInputAndGetSeeds(file);
 
         long minLocation = Long.MAX_VALUE;
-
-        String[] seeds = split[0].split(" ");
         for (int i = 1; i < seeds.length; i++) {
             long current = parseLong(seeds[i]);
             for (String string : types) {
@@ -64,6 +55,27 @@ public class Advent5 {
     }
 
     public Long runP2(String file) {
+        String[] seeds = parseInputAndGetSeeds(file);
+
+        List<Interval> intervals = newArrayList();
+        for (int seed = 1; seed < seeds.length; seed += 2) {
+            intervals.add(new Interval("seed", parseLong(seeds[seed]), parseLong(seeds[seed]) + parseLong(seeds[seed + 1]) - 1));
+        }
+
+        // apply each set of conversions in order
+        for (int type = 1; type < types.size(); type++) {
+            String convertTo = types.get(type);
+            List<Conversion> list = conversions.get(convertTo);
+            intervals = applyConversions(list, intervals, convertTo);
+        }
+
+        return intervals.stream()
+                .sorted(comparing(o -> o.start))
+                .findFirst().orElseThrow()
+                .start;
+    }
+
+    private String[] parseInputAndGetSeeds(String file) {
         String[] split = Util.fileStream(file)
                 .collect(Collectors.joining("\n"))
                 .split("\n\n");
@@ -75,33 +87,20 @@ public class Advent5 {
 
             Arrays.stream(lines).toList().subList(1, lines.length).forEach(line -> {
                 String[] entry = line.split(" ");
+                long start = parseLong(entry[1]);
+                long end = start + parseLong(entry[2]) - 1;
                 conversions.get(mapType)
-                        .add(new Conversion(mapType, parseLong(entry[0]), parseLong(entry[1]), parseLong(entry[1]) + parseLong(entry[2]) - 1));
+                        .add(new Conversion(mapType, parseLong(entry[0]), start, end));
             });
         }
 
-        String[] seeds = split[0].split(" ");
-
-        List<Interval> intervals = newArrayList();
-        for (int seed = 1; seed < seeds.length; seed += 2) {
-            intervals.add(new Interval("seed", parseLong(seeds[seed]), parseLong(seeds[seed]) + parseLong(seeds[seed + 1]) - 1));
-        }
-
-        for (int type = 1; type < types.size(); type++) {
-            String convertTo = types.get(type);
-            List<Conversion> list = conversions.get(convertTo);
-            intervals = convert(list, intervals, convertTo);
-        }
-
-        return intervals.stream()
-                .sorted(Comparator.comparing(o -> o.start))
-                .findFirst().get()
-                .start;
+        return split[0].split(" ");
     }
 
-    private List<Interval> convert(List<Conversion> conversions, List<Interval> intervals, String to) {
+    private List<Interval> applyConversions(List<Conversion> conversions, List<Interval> intervals, String to) {
         List<Interval> newIntervals = newArrayList();
 
+        // break down intervals into smaller ones so that an interval is either fully contained or fully outside a conversion
         intervals.forEach(interval -> {
             ArrayList<Interval> result = newArrayList();
             breakDownInterval(conversions, interval, result);
@@ -109,18 +108,13 @@ public class Advent5 {
         });
 
         List<Interval> result = newArrayList();
-        newIntervals.forEach(interval -> {
-            Optional<Conversion> first = conversions.stream()
-                    .filter(conversion -> conversion.start <= interval.start && conversion.end >= interval.end)
-                    .findFirst();
 
-            if (first.isPresent()) {
-                result.add(convertInterval(interval, first.get()));
-            } else {
-                result.add(interval.withType(to));
-            }
-        });
-
+        // for each interval, find the conversion that contains it, then convert it, otherwise keep it as is
+        newIntervals.forEach(interval -> conversions.stream()
+                .filter(conversion -> conversion.containsInterval(interval))
+                .findFirst()
+                .ifPresentOrElse(conversion -> result.add(convertInterval(interval, conversion)),
+                        () -> result.add(interval.withType(to))));
         return result;
     }
 
@@ -135,23 +129,28 @@ public class Advent5 {
         List<Long> breakpoints = newArrayList();
 
         conversions.stream()
-                .sorted(Comparator.comparing(o -> o.start))
+                .sorted(comparing(o -> o.start))
                 .filter(conversion -> interval.contains(conversion.start))
                 .forEach(conversion -> breakpoints.add(conversion.start));
         conversions.stream()
-                .sorted(Comparator.comparing(Conversion::end))
+                .sorted(comparing(Conversion::end))
                 .filter(conversion -> interval.contains(conversion.end))
                 .forEach(conversion -> breakpoints.add(conversion.end));
 
         breakpoints.sort(Long::compare);
 
-        AtomicBoolean requiresBreak = new AtomicBoolean(false);
-        final Long[] prev = {interval.start};
+        result.addAll(splitIntervalAtBreakpoints(interval, breakpoints));
+    }
+
+    private List<Interval> splitIntervalAtBreakpoints(Interval interval, List<Long> breakpoints) {
+        boolean requiresBreak = false;
+        long prev = interval.start;
+        List<Interval> result = newArrayList();
 
         for (int b = 0; b < breakpoints.size(); b++) {
             Long breakpoint = breakpoints.get(b);
 
-            result.add(new Interval(interval.type, prev[0], breakpoint - 1));
+            result.add(new Interval(interval.type, prev, breakpoint - 1));
 
             Long tempBreak = interval.end;
             if (b != breakpoints.size() - 1) {
@@ -159,13 +158,16 @@ public class Advent5 {
             }
             long end = min(interval.end, tempBreak);
             result.add(new Interval(interval.type, breakpoint, end));
-            prev[0] = breakpoint;
-            requiresBreak.set(true);
+
+            prev = breakpoint;
+            requiresBreak = true;
         }
 
-        if (!requiresBreak.get()) {
+        if (!requiresBreak) {
             result.add(interval);
         }
+
+        return result;
     }
 
     @With
@@ -179,6 +181,10 @@ public class Advent5 {
         @Override
         public String toString() {
             return start + "-" + end + " -> " + destination + "-" + (destination + end - start);
+        }
+
+        boolean containsInterval(Interval interval) {
+            return this.start <= interval.start && this.end >= interval.end;
         }
     }
 }
