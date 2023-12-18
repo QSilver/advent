8eef3458ea4c0b22cd65f306e27bfd91ee017d9e
QSilver <geo_007.popa@yahoo.com>
2023-12-18 11:55:41 +0000

    2023 Day 18 - cleanup

--- src/advent2023/Advent18.java
+++ src/advent2023/Advent18.java
@@ -2,9 +2,9 @@ package advent2023;
 
 import lombok.extern.slf4j.Slf4j;
 import util.Util.Point;
-import util.Util.Surface;
 
 import java.util.List;
+import java.util.function.Function;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static java.lang.Integer.parseInt;
@@ -16,43 +16,35 @@ public class Advent18 {
     // https://adventofcode.com/2023/day/18
 
     public Long runP1(String file) {
-
-        List<Point> points = newArrayList();
-        points.add(new Point(0, 0));
-
-        fileStream(file).forEach(line -> {
-            String[] split = line.split(" ");
-
+        return extractPointsAndCalculateArea(file, split -> {
             Direction direction = Direction.valueOf(split[0]);
             int digSize = parseInt(split[1]);
-
-            points.add(getNextPoint(points.getLast(), direction, digSize));
+            return new Result(direction, digSize);
         });
-
-        Surface surface = calculateSurface(points);
-        return surface.area();
     }
 
     public Long runP2(String file) {
-        List<String> list = fileStream(file).toList();
-
-        List<Point> points = newArrayList();
-        points.add(new Point(0, 0));
-
-        list.forEach(line -> {
-            String[] split = line.split(" ");
-
+        return extractPointsAndCalculateArea(file, split -> {
             String hex = split[2];
             int lastDigit = parseInt(hex.substring(hex.length() - 2, hex.length() - 1));
 
             Direction direction = Direction.values()[lastDigit];
             int digSize = parseInt(hex.substring(2, hex.length() - 2), 16);
 
-            points.add(getNextPoint(points.getLast(), direction, digSize));
+            return new Result(direction, digSize);
+        });
+    }
+
+    private long extractPointsAndCalculateArea(String file, Function<String[], Result> coordinateExtractor) {
+        List<Point> points = newArrayList(new Point(0, 0));
+
+        fileStream(file).forEach(line -> {
+            Result result = coordinateExtractor.apply(line.split(" "));
+            Point nextPoint = getNextPoint(points.getLast(), result.direction(), result.digSize);
+            points.add(nextPoint);
         });
 
-        Surface surface = calculateSurface(points);
-        return surface.area();
+        return calculateSurface(points).area();
     }
 
     private static Point getNextPoint(Point last, Direction direction, int digSize) {
@@ -71,6 +63,9 @@ public class Advent18 {
             }
         }
         return null;
+    }
+
+    private record Result(Direction direction, int digSize) {
     }
 
     enum Direction {
