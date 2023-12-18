aab5b8063614fce2e63ce614535e38515c17851f
QSilver <geo_007.popa@yahoo.com>
2023-12-18 10:38:59 +0000

    2023 Day 19 prep

---
+++ src/advent2023/Advent19.java
@@ -0,0 +1,23 @@
+package advent2023;
+
+import lombok.extern.slf4j.Slf4j;
+import util.Util;
+
+import java.util.List;
+
+@Slf4j
+public class Advent19 {
+    // https://adventofcode.com/2023/day/19
+
+    public Long runP1(String file) {
+        List<String> list = Util.fileStream(file).toList();
+
+        return 0L;
+    }
+
+    public Long runP2(String file) {
+        List<String> list = Util.fileStream(file).toList();
+
+        return 0L;
+    }
+}
