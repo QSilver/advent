c6094b3d8d53450abb5f3b66c212111c60d34362
QSilver <geo_007.popa@yahoo.com>
2023-12-22 14:49:15 +0000

    2023 Day 22 - rewritten to be more object-oriented

--- src/advent2023/Advent22.java
+++ src/advent2023/Advent22.java
@@ -20,7 +20,7 @@ import static util.Util.fileStream;
 @Slf4j
 public class Advent22 {
     // https://adventofcode.com/2023/day/22
-    static String brickIDs = "ABCDEFG";
+    static final String BRICK_IDS = "ABCDEFG";
     static int id = 0;
 
     public Long runP1(String file, boolean withRandomIds) {
@@ -30,89 +30,28 @@ public class Advent22 {
     public Long runP2(String file, boolean withRandomIds) {
         Result result = getCanDisintegrate(file, withRandomIds);
         return (long) result.zSortedBricks.stream()
+                // consider only bricks that would cause falling
                 .filter(brick -> !result.canDisintegrate.contains(brick))
-                .mapToInt(this::getAllBricksRestingOn)
+                .mapToInt(Brick::getAllBricksRestingOn)
                 .sum();
     }
 
     private Result getCanDisintegrate(String file, boolean withRandomIds) {
         List<Brick> bricks = getSnapshotSortedByZ(file, withRandomIds);
-        bricks.forEach(brick -> fall(brick, bricks));
+        bricks.forEach(brick -> brick.fall(bricks));
 
         // can disintegrate all bricks that don't have anything above
         Set<Brick> canDisintegrate = bricks.stream()
                 .filter(brick -> brick.above.isEmpty())
                 .collect(Collectors.toSet());
 
-        // can disintegrate a bricks in the case where every other brick that depends on it has at least 1 other support
+        // can disintegrate a brick in the case where every other brick directly above has at least 1 other support
         bricks.forEach(brick -> {
             if (brick.above.stream().allMatch(Brick::hasMoreThanOneSupport)) {
                 canDisintegrate.add(brick);
             }
         });
         return new Result(canDisintegrate, bricks);
-    }
-
-    private int getAllBricksRestingOn(Brick brick) {
-        Set<Brick> willFall = newHashSet(brick);
-
-        Queue<Brick> toProcess = newArrayDeque();
-        toProcess.add(brick);
-
-        while (!toProcess.isEmpty()) {
-            Brick current = toProcess.poll();
-            // get resting on current brick, check which ones rest on something else that won't fall
-            for (Brick dependent : current.above) {
-                if (isRestingOnAnyOtherBrickThatWillNotFall(dependent, willFall)) {
-                    willFall.add(dependent);
-                    toProcess.add(dependent);
-                }
-            }
-        }
-
-        // remove original brick since that doesn't fall, but gets disintegrated
-        return willFall.size() - 1;
-    }
-
-    private boolean isRestingOnAnyOtherBrickThatWillNotFall(Brick dependent, Set<Brick> willFall) {
-        Set<Brick> bricksThisOneRestsOn = newHashSet(dependent.below);
-        bricksThisOneRestsOn.removeAll(willFall);
-        return bricksThisOneRestsOn.isEmpty();
-    }
-
-    void fall(Brick current, List<Brick> brickList) {
-
-        int currentWillFallToZ = 0;
-        Set<Brick> willFallOn = newHashSet();
-
-        for (Brick other : brickList) {
-            // only look at bricks that are under this one since you can't fall down on a brick above you
-            if (current == other) {
-                break;
-            }
-
-            // if points in the projection of the brick overlap, then the falling brick will touch another
-            if (!disjoint(current.surface, other.surface)) {
-                int heightOfTower = other.getMaxZ() + 1;
-
-                // we've discovered an intersecting brick higher than the previously found one
-                // forget about that and start again from the new highest point
-                if (heightOfTower > currentWillFallToZ) {
-                    willFallOn.clear();
-                    willFallOn.add(other);
-                    currentWillFallToZ = heightOfTower;
-                } else if (heightOfTower == currentWillFallToZ) {
-                    // there is more than one brick at the top level
-                    // currently falling one will rest on more than one brick
-                    willFallOn.add(other);
-                }
-            }
-        }
-
-        current.fallDownTo(currentWillFallToZ);
-
-        current.below.addAll(willFallOn);
-        willFallOn.forEach(supporting -> supporting.above.add(current));
     }
 
     private static List<Brick> getSnapshotSortedByZ(String file, boolean withRandomIds) {
@@ -152,35 +91,91 @@ public class Advent22 {
                     .collect(Collectors.toSet());
         }
 
-        private void generateId(boolean withRandomIds) {
+        void generateId(boolean withRandomIds) {
             if (withRandomIds) {
                 this.uuid = randomUUID().toString();
             } else {
-                this.uuid = brickIDs.charAt(id++ % brickIDs.length()) + "";
+                this.uuid = BRICK_IDS.charAt(id++ % BRICK_IDS.length()) + "";
             }
         }
 
-        public boolean hasMoreThanOneSupport() {
+        boolean hasMoreThanOneSupport() {
             return below.size() > 1;
         }
 
-        public int getMaxZ() {
+        int getAllBricksRestingOn() {
+            Set<Brick> willFall = newHashSet(this);
+
+            Queue<Brick> toProcess = newArrayDeque();
+            toProcess.add(this);
+
+            while (!toProcess.isEmpty()) {
+                Brick current = toProcess.poll();
+                // get resting on current brick, check which ones rest on something else that won't fall
+                current.above.stream()
+                        .filter(dependent -> dependent.isRestingOnAnyOtherBrickThatWillNotFall(willFall))
+                        .forEach(dependent -> {
+                            willFall.add(dependent);
+                            toProcess.add(dependent);
+                        });
+            }
+
+            // remove original brick since that doesn't fall, but gets disintegrated
+            return willFall.size() - 1;
+        }
+
+        boolean isRestingOnAnyOtherBrickThatWillNotFall(Set<Brick> falling) {
+            Set<Brick> bricksThisOneRestsOn = newHashSet(below);
+            bricksThisOneRestsOn.removeAll(falling);
+            return bricksThisOneRestsOn.isEmpty();
+        }
+
+        void fall(List<Brick> brickList) {
+            int currentWillFallToZ = 0;
+            Set<Brick> willFallOn = newHashSet();
+
+            for (Brick other : brickList) {
+                // only look at bricks that are under this one since you can't fall down on a brick above you
+                if (this == other) {
+                    break;
+                }
+
+                // if points in the projection of the brick overlap, then the falling brick will touch another
+                if (!disjoint(surface, other.surface)) {
+                    int heightOfTower = other.getMaxZ() + 1;
+                    if (heightOfTower > currentWillFallToZ) {
+                        // we've discovered an intersecting brick higher than the previously found one
+                        // forget about the old one and start again from the new highest point
+                        willFallOn.clear();
+                        willFallOn.add(other);
+                        currentWillFallToZ = heightOfTower;
+                    } else if (heightOfTower == currentWillFallToZ) {
+                        // there is more than one brick at the top level
+                        // currently falling one will rest on more than one brick
+                        willFallOn.add(other);
+                    }
+                }
+            }
+
+            int deltaZ = getMinZ() - currentWillFallToZ;
+            points = points.stream()
+                    .map(point3D -> new Point3D(point3D.x(), point3D.y(), point3D.z() - deltaZ))
+                    .collect(Collectors.toSet());
+
+            below.addAll(willFallOn);
+            willFallOn.forEach(supporting -> supporting.above.add(this));
+        }
+
+        int getMaxZ() {
             return points.stream()
                     .mapToInt(point3D -> (int) point3D.z())
                     .max().orElseThrow();
         }
 
-        public int getMinZ() {
+        int getMinZ() {
             return points.stream()
                     .mapToInt(point3D -> (int) point3D.z())
                     .min().orElseThrow();
-        }
-
-        public void fallDownTo(int fallDownToZ) {
-            int deltaZ = getMinZ() - fallDownToZ;
-            points = points.stream()
-                    .map(point3D -> new Point3D(point3D.x(), point3D.y(), point3D.z() - deltaZ))
-                    .collect(Collectors.toSet());
         }
 
         @Override
