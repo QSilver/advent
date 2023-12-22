5ff3efa58f60f34179a848091eb24dc98b95b349
QSilver <geo_007.popa@yahoo.com>
2023-12-22 12:02:52 +0000

    2023 Day 23 - prep

---
+++ src/advent2023/Advent23.java
@@ -0,0 +1,18 @@
+package advent2023;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Advent23 {
+    // https://adventofcode.com/2023/day/23
+
+    public Long runP1(String file) {
+
+        return 0L;
+    }
+
+    public Long runP2(String file) {
+
+        return 0L;
+    }
+}
---
+++ test/advent2023/Advent23Test.java
@@ -0,0 +1,33 @@
+package advent2023;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Advent23Test {
+    private final Advent23 underTest = new Advent23();
+
+    @Test
+    void testPart1() {
+        Long run = underTest.runP1("advent2023/advent23_ex1.in");
+        assertEquals(0, run);
+    }
+
+    @Test
+    void runPart1() {
+        Long run = underTest.runP1("advent2023/advent23.in");
+        System.out.println(run);
+    }
+
+    @Test
+    void testPart2() {
+        Long run = underTest.runP2("advent2023/advent23_ex1.in");
+        assertEquals(0, run);
+    }
+
+    @Test
+    void runPart2() {
+        Long run = underTest.runP2("advent2023/advent23.in");
+        System.out.println(run);
+    }
+}
