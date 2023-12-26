9f41e1708769cb4a8d06b0803f6609c5ef7ca3d9
QSilver <geo_007.popa@yahoo.com>
2023-12-26 15:21:18 +0000

    at some point I broke day 10 when refactor

--- src/advent2023/Advent10.java
+++ src/advent2023/Advent10.java
@@ -2,11 +2,13 @@ package advent2023;
 
 import lombok.extern.slf4j.Slf4j;
 import util.Util;
+import util.Util.Point;
+import util.Util.Surface;
 
 import java.util.List;
 
 import static com.google.common.collect.Lists.newArrayList;
-import static java.lang.Math.abs;
+import static util.Util.calculateSurface;
 
 
 @Slf4j
@@ -17,62 +19,54 @@ public class Advent10 {
     public static final String LEFT = "-LF";
     public static final String RIGHT = "-J7";
     char[][] map;
-    List<Point> path = newArrayList();
 
     public Long runP1(String file) {
-        Point start = parseInputAndGetStartPoint(file);
+        PointWithSymbol start = parseInputAndGetStartPoint(file);
 
-        buildPath(start.row, start.col);
+        List<PointWithSymbol> path = buildPath((int) start.p.row(), (int) start.p.col());
         path.addFirst(start);
 
         return path.size() / 2L;
     }
 
     public Long runP2(String file) {
-        Point start = parseInputAndGetStartPoint(file);
+        PointWithSymbol start = parseInputAndGetStartPoint(file);
 
-        buildPath(start.row, start.col);
+        List<PointWithSymbol> path = buildPath((int) start.p.row(), (int) start.p.col());
         path.addFirst(start);
 
-        // https://en.m.wikipedia.org/wiki/Shoelace_formula
-        long area = 0L;
-        for (int i = 0; i < path.size() - 1; i++) {
-            area += (((long) path.get(i).row * path.get(i + 1).col) - ((long) path.get(i + 1).row * path.get(i).col));
-        }
-        area += (((long) path.getLast().row * path.getFirst().col) - ((long) path.getFirst().row * path.getLast().col));
-        area = abs(area) / 2L;
-
-        // https://en.m.wikipedia.org/wiki/Pick%27s_theorem
-        return area + 1 - path.size() / 2;
+        Surface surface = calculateSurface(path.stream().map(pointWithSymbol -> pointWithSymbol.p).toList());
+        return surface.insidePoints();
     }
 
-    private Point parseInputAndGetStartPoint(String file) {
+    private PointWithSymbol parseInputAndGetStartPoint(String file) {
         List<String> list = Util.fileStream(file).toList();
 
-        Point start = new Point(-1, -1, "#");
+        PointWithSymbol start = new PointWithSymbol(new Point(-1, -1, 0), "#");
 
         map = new char[list.size() + 2][list.getFirst().length() + 2];
         for (int row = 0; row < list.size(); row++) {
             for (int col = 0; col < list.get(row).length(); col++) {
                 map[row + 1][col + 1] = list.get(row).charAt(col);
                 if (map[row + 1][col + 1] == 'S') {
-                    start = new Point(row + 1, col + 1, "S");
+                    start = new PointWithSymbol(new Point(row + 1, col + 1, 0), "S");
                 }
             }
         }
         return start;
     }
 
-    void buildPath(int row, int col) {
+    List<PointWithSymbol> buildPath(int row, int col) {
         int initialRow = row;
         int initialCol = col;
         String prev = "S";
 
+        List<PointWithSymbol> path = newArrayList();
         do {
-            Point up = new Point(row - 1, col, map[row - 1][col] + "");
-            Point down = new Point(row + 1, col, map[row + 1][col] + "");
-            Point left = new Point(row, col - 1, map[row][col - 1] + "");
-            Point right = new Point(row, col + 1, map[row][col + 1] + "");
+            PointWithSymbol up = new PointWithSymbol(new Point(row - 1, col, 0), map[row - 1][col] + "");
+            PointWithSymbol down = new PointWithSymbol(new Point(row + 1, col, 0), map[row + 1][col] + "");
+            PointWithSymbol left = new PointWithSymbol(new Point(row, col - 1, 0), map[row][col - 1] + "");
+            PointWithSymbol right = new PointWithSymbol(new Point(row, col + 1, 0), map[row][col + 1] + "");
 
             if (UP.contains(up.symbol()) && ("S" + DOWN).contains(prev) && !path.contains(up)) {
                 path.add(up);
@@ -82,20 +76,22 @@ public class Advent10 {
                 path.add(down);
                 prev = down.symbol;
                 row++;
-            } else if (LEFT.contains(left.symbol) && ("S" + LEFT).contains(prev) && !path.contains(left)) {
+            } else if (LEFT.contains(left.symbol) && ("S" + RIGHT).contains(prev) && !path.contains(left)) {
                 path.add(left);
                 prev = left.symbol;
                 col--;
-            } else if (RIGHT.contains(right.symbol) && ("S" + RIGHT).contains(prev) && !path.contains(right)) {
+            } else if (RIGHT.contains(right.symbol) && ("S" + LEFT).contains(prev) && !path.contains(right)) {
                 path.add(right);
                 prev = right.symbol;
                 col++;
             } else {
-                return;
+                break;
             }
         } while (!(row == initialRow && col == initialCol));
+        return path;
     }
 
-    record Point(int row, int col, String symbol) {
+    record PointWithSymbol(Point p, String symbol) {
+
     }
 }
