5ce96a00352723bccafa4aa6cedf208620ebde13
QSilver <geo_007.popa@yahoo.com>
2024-12-02 06:28:57 +0000

    cleanup Day 1 + boilerplate Day 3

--- src/advent2024/Advent1.java
+++ src/advent2024/Advent1.java
@@ -11,40 +11,41 @@ import static util.InputUtils.fileStream;
 
 @Slf4j
 public class Advent1 {
-
-    public static final String DELIMITER = " {3}";
-
     // https://adventofcode.com/2024/day/1
+
     public Integer runP1(String file) {
-        List<Integer> first = newArrayList();
-        List<Integer> second = newArrayList();
+        Result result = parseInput(file);
 
-        fileStream(file).forEach(line -> {
-            String[] s = line.split(DELIMITER);
-            first.add(Integer.parseInt(s[0]));
-            second.add(Integer.parseInt(s[1]));
-        });
+        result.first.sort(Integer::compareTo);
+        result.second.sort(Integer::compareTo);
 
-        first.sort(Integer::compareTo);
-        second.sort(Integer::compareTo);
-
-        return IntStream.range(0, first.size())
-                .map(index -> Math.abs(first.get(index) - second.get(index)))
+        return IntStream.range(0, result.first.size())
+                .map(index -> Math.abs(result.first.get(index) - result.second.get(index)))
                 .sum();
     }
+
 
     public Integer runP2(String file) {
+        Result result = parseInput(file);
+
+        return result.first.stream()
+                .mapToInt(integer -> integer * frequency(result.second, integer))
+                .sum();
+    }
+
+    private static Result parseInput(String file) {
         List<Integer> first = newArrayList();
         List<Integer> second = newArrayList();
 
         fileStream(file).forEach(line -> {
-            String[] s = line.split(DELIMITER);
+            String[] s = line.split(" {3}");
             first.add(Integer.parseInt(s[0]));
             second.add(Integer.parseInt(s[1]));
         });
-
-        return first.stream()
-                .mapToInt(integer -> integer * frequency(second, integer))
-                .sum();
+        return new Result(first, second);
     }
+
+    private record Result(List<Integer> first, List<Integer> second) {
+    }
+
 }
---
+++ src/advent2024/Advent3.java
@@ -0,0 +1,20 @@
+package advent2024;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static util.InputUtils.fileStream;
+
+@Slf4j
+public class Advent3 {
+    // https://adventofcode.com/2024/day/3
+
+    public Integer runP1(String file) {
+        fileStream(file);
+        return 0;
+    }
+
+    public Integer runP2(String file) {
+        fileStream(file);
+        return 0;
+    }
+}
