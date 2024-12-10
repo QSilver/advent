a9af945fae985cac2110aedc7bc807fe4975178c
QSilver <geo_007.popa@yahoo.com>
2024-12-10 06:03:33 +0000

    refactor Day 10

--- src/advent2024/Advent10.java
+++ src/advent2024/Advent10.java
@@ -6,7 +6,7 @@ import util.Extensions;
 import util.Util2D.Point2D;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static util.Util2D.loadIntMatrix;
@@ -19,35 +19,30 @@ public class Advent10 {
     int[][] map = new int[0][0];
 
     public Long runP1(String file) {
-        map = loadIntMatrix(file);
-        List<Point2D> starts = findStart(map);
-
-        AtomicLong globalCount = new AtomicLong();
-        starts.forEach(start -> {
-            List<List<Point2D>> paths = newArrayList();
-            paths.add(newArrayList(start));
-
-            int count = 0;
-            for (int height = 0; height <= 9; height++) {
-                if (height == 9) {
-                    count += (int) paths.stream().map(List::getLast).distinct().count();
-                }
-
-                List<List<Point2D>> iter = newArrayList();
-                for (List<Point2D> path : paths) {
-                    iter.addAll(getPaths(path.getLast(), height, path));
-                }
-                paths.clear();
-                paths.addAll(iter);
-            }
-
-            globalCount.addAndGet(count);
-        });
-
-        return globalCount.get();
+        return run(file, countEndpoints());
     }
 
-    private static List<Point2D> findStart(int[][] map) {
+
+    public Long runP2(String file) {
+        return run(file, countPaths());
+    }
+
+    private long run(String file, Function<List<List<Point2D>>, Integer> countingFunction) {
+        map = loadIntMatrix(file);
+        return findStartingPoints(map).stream()
+                .mapToInt(start -> tracePaths(start, countingFunction))
+                .sum();
+    }
+
+    private static Function<List<List<Point2D>>, Integer> countEndpoints() {
+        return paths -> (int) paths.stream().map(List::getLast).distinct().count();
+    }
+
+    private static Function<List<List<Point2D>>, Integer> countPaths() {
+        return List::size;
+    }
+
+    private static List<Point2D> findStartingPoints(int[][] map) {
         List<Point2D> points = newArrayList();
         for (int row = 0; row < map.length; row++) {
             for (int col = 0; col < map[row].length; col++) {
@@ -57,6 +52,29 @@ public class Advent10 {
             }
         }
         return points;
+    }
+
+    private int tracePaths(Point2D start, Function<List<List<Point2D>>, Integer> countEndpoints) {
+        List<List<Point2D>> paths = List.of(List.of(start));
+
+        int count = 0;
+        for (int height = 0; height <= 9; height++) {
+            if (height == 9) {
+                count += countEndpoints.apply(paths);
+            }
+            getNextPaths(paths, height);
+        }
+        return count;
+    }
+
+    private void getNextPaths(List<List<Point2D>> paths, int height) {
+        List<List<Point2D>> iterationStep = newArrayList();
+        for (List<Point2D> path : paths) {
+            iterationStep.addAll(getPaths(path.getLast(), height, path));
+        }
+
+        paths.clear();
+        paths.addAll(iterationStep);
     }
 
     private List<List<Point2D>> getPaths(Point2D current, int currentHeight, List<Point2D> currentPath) {
@@ -69,40 +87,10 @@ public class Advent10 {
                     pathCopy.add(neighbour);
                     futurePaths.add(pathCopy);
                 }
-            } catch (Exception _) {
-
+            } catch (ArrayIndexOutOfBoundsException _) {
             }
         });
 
         return futurePaths;
-    }
-
-    public Long runP2(String file) {
-        map = loadIntMatrix(file);
-        List<Point2D> starts = findStart(map);
-
-        AtomicLong globalCount = new AtomicLong();
-        starts.forEach(start -> {
-            List<List<Point2D>> paths = newArrayList();
-            paths.add(newArrayList(start));
-
-            int count = 0;
-            for (int height = 0; height <= 9; height++) {
-                if (height == 9) {
-                    count += paths.size();
-                }
-
-                List<List<Point2D>> iter = newArrayList();
-                for (List<Point2D> path : paths) {
-                    iter.addAll(getPaths(path.getLast(), height, path));
-                }
-                paths.clear();
-                paths.addAll(iter);
-            }
-
-            globalCount.addAndGet(count);
-        });
-
-        return globalCount.get();
     }
 }
