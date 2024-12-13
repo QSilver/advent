46260ad10e86aad5f45d28de26aca712dada4123
QSilver <geo_007.popa@yahoo.com>
2024-12-13 07:23:21 +0000

    refactor Day 13

--- src/advent2024/Advent13.java
+++ src/advent2024/Advent13.java
@@ -5,11 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import util.Extensions;
 
 import java.util.regex.Matcher;
-import java.util.stream.Collectors;
 
 import static java.lang.Integer.parseInt;
 import static java.util.regex.Pattern.compile;
-import static util.InputUtils.fileStream;
+import static util.InputUtils.readDoubleNewlineBlocks;
 
 @Slf4j
 @ExtensionMethod({Extensions.class})
@@ -19,33 +18,34 @@ public class Advent13 {
     private static long extra = 0L;
 
     public Long runP1(String file) {
-        return fileStream(file).collect(Collectors.joining("SEPARATOR")).split("SEPARATORSEPARATOR").stream()
-                .mapToLong(Advent13::calculateButtons)
-                .sum();
+        return run(file);
     }
 
     public Long runP2(String file) {
         extra = 10000000000000L;
-        return fileStream(file).collect(Collectors.joining("SEPARATOR")).split("SEPARATORSEPARATOR").stream()
+        return run(file);
+    }
+
+    private static long run(String file) {
+        return readDoubleNewlineBlocks(file).stream()
                 .mapToLong(Advent13::calculateButtons)
                 .sum();
     }
 
     private static long calculateButtons(String clawMachine) {
-        Matcher matcher = compile("\\d+").matcher(clawMachine);
+        Matcher matcher = compile("""
+                Button A: X\\+(\\d+), Y\\+(\\d+)
+                Button B: X\\+(\\d+), Y\\+(\\d+)
+                Prize: X=(\\d+), Y=(\\d+)""")
+                .matcher(clawMachine);
         matcher.find();
-        long ax = parseInt(matcher.group());
-        matcher.find();
-        long ay = parseInt(matcher.group());
-        matcher.find();
-        long bx = parseInt(matcher.group());
-        matcher.find();
-        long by = parseInt(matcher.group());
-        matcher.find();
-        long px = parseInt(matcher.group()) + extra;
-        matcher.find();
-        long py = parseInt(matcher.group()) + extra;
-        Result result = new Result(ax, ay, bx, by, px, py);
+
+        long ax = parseInt(matcher.group(1));
+        long ay = parseInt(matcher.group(2));
+        long bx = parseInt(matcher.group(3));
+        long by = parseInt(matcher.group(4));
+        long px = parseInt(matcher.group(5)) + extra;
+        long py = parseInt(matcher.group(6)) + extra;
 
         /*
          ax A + bx B = px
@@ -61,15 +61,12 @@ public class Advent13 {
          B = (py ax - px ay) / (ax by - ay bx)
          */
 
-        long B = (result.py() * result.ax() - result.px() * result.ay()) / (result.ax() * result.by() - result.ay() * result.bx());
-        long A = (result.px() - result.bx() * B) / result.ax();
+        long B = (py * ax - px * ay) / (ax * by - ay * bx);
+        long A = (px - bx * B) / ax;
 
-        if (result.ax() * A + result.bx() * B != result.px() || result.ay() * A + result.by() * B != result.py()) {
+        if (ax * A + bx * B != px || ay * A + by * B != py) {
             return 0;
         }
         return A * 3 + B;
-    }
-
-    private record Result(long ax, long ay, long bx, long by, long px, long py) {
     }
 }
