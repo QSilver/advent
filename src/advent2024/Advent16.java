295bebdd4121579f381c8dcf56a487adb9bd7677
QSilver <geo_007.popa@yahoo.com>
2024-12-16 14:50:48 +0000

    refactor Day 16

--- src/advent2024/Advent16.java
+++ src/advent2024/Advent16.java
@@ -1,20 +1,21 @@
 package advent2024;
 
-import lombok.With;
 import lombok.experimental.ExtensionMethod;
 import lombok.extern.slf4j.Slf4j;
 import util.Extensions;
-import util.Util.Direction;
+import util.Util.Node;
 import util.Util2D.Point2D;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Maps.newHashMap;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.util.Comparator.comparingLong;
 import static util.Util.Direction.*;
+import static util.Util.getAllPaths;
 import static util.Util2D.get2DPoints;
 import static util.Util2D.loadCharMatrix;
 
@@ -26,21 +27,21 @@ public class Advent16 {
     private static Character[][] map;
 
     public Long runP1(String file) {
-        map = loadCharMatrix(file);
-        Point2D start = get2DPoints(file, 'S').getFirst();
-        Point2D end = get2DPoints(file, 'E').getFirst();
-
-        Node startRightNode = new Node((int) start.row(), (int) start.col(), RIGHT, 0, null);
-
-        Function<Node, List<Node>> neighbourFunction = this::getNeighbours;
-        Function<Node, Boolean> endCondition = current -> (current.row == end.row() && current.col == end.col());
-        Comparator<Node> sorting = comparingLong(value -> value.distance);
-
-        List<Node> paths = getAllPaths(startRightNode, endCondition, neighbourFunction, sorting, _ -> 0);
-        return paths.getFirst().distance;
+        return getPaths(file).getFirst().distance();
     }
 
     public Long runP2(String file) {
+        List<Node> paths = getPaths(file);
+
+        long minDistance = paths.getFirst().distance();
+        return paths.stream()
+                .filter(node -> node.distance() == minDistance)
+                .flatMap(Advent16::traverse)
+                .distinct()
+                .count();
+    }
+
+    private List<Node> getPaths(String file) {
         map = loadCharMatrix(file);
         Point2D start = get2DPoints(file, 'S').getFirst();
         Point2D end = get2DPoints(file, 'E').getFirst();
@@ -48,133 +49,55 @@ public class Advent16 {
         Node startRightNode = new Node((int) start.row(), (int) start.col(), RIGHT, 0, null);
 
         Function<Node, List<Node>> neighbourFunction = this::getNeighbours;
-        Function<Node, Boolean> endCondition = current -> (current.row == end.row() && current.col == end.col());
-        Comparator<Node> sorting = comparingLong(value -> value.distance);
+        Function<Node, Boolean> endCondition = current -> (current.row() == end.row() && current.col() == end.col());
+        Comparator<Node> sorting = comparingLong(Node::distance);
 
-        List<Node> paths = getAllPaths(startRightNode, endCondition, neighbourFunction, sorting, _ -> 0);
-        long min = paths.getFirst().distance;
+        return getAllPaths(startRightNode, endCondition, neighbourFunction, sorting);
+    }
 
+    private static Stream<Point2D> traverse(Node node) {
         Set<Point2D> onBestPath = newHashSet();
-        paths.stream()
-                .filter(node -> node.distance == min)
-                .forEach(node -> {
-                    Node traversal = node;
-                    while (traversal != null) {
-                        onBestPath.add(new Point2D(traversal.row, traversal.col));
-                        traversal = traversal.previous;
-                    }
-                });
-
-        return (long) onBestPath.size();
-    }
-
-    Node getShortestPath(Node from, Function<Node, Boolean> endCondition, Function<Node, List<Node>> neighbourFunction, Comparator<Node> sorting, Function<Node, Integer> distanceFunction) {
-        PriorityQueue<Node> toVisit = new PriorityQueue<>(sorting);
-        Set<Node> seen = newHashSet();
-
-        toVisit.add(from);
-        Node end = null;
-        while (end == null) {
-            if (toVisit.isEmpty()) {
-                break;
-            }
-
-            Node current = toVisit.remove();
-            end = endCondition.apply(current) ? current : null;
-
-            List<Node> neighbours = neighbourFunction.apply(current)
-                    .stream().filter(node -> !seen.contains(node))
-                    .toList();
-
-            toVisit.addAll(neighbours);
-            seen.addAll(neighbours);
+        Node traversal = node;
+        while (traversal != null) {
+            onBestPath.add(new Point2D(traversal.row(), traversal.col()));
+            traversal = traversal.previous();
         }
-
-        return end;
-    }
-
-    List<Node> getAllPaths(Node from, Function<Node, Boolean> endCondition, Function<Node, List<Node>> neighbourFunction, Comparator<Node> sorting, Function<Node, Integer> distanceFunction) {
-        List<Node> paths = newArrayList();
-
-        PriorityQueue<Node> toVisit = new PriorityQueue<>(sorting);
-        Map<Node, Long> seen = newHashMap();
-
-        toVisit.add(from);
-        while (!toVisit.isEmpty()) {
-            Node current = toVisit.remove();
-
-            if (endCondition.apply(current)) {
-                paths.add(current);
-            }
-
-            List<Node> apply = neighbourFunction.apply(current);
-            List<Node> neighbours = apply
-                    .stream().filter(node -> seen.getOrDefault(node, Long.MAX_VALUE) > current.distance)
-                    .toList();
-
-            toVisit.addAll(neighbours);
-            neighbours.forEach(node -> seen.put(node, node.distance));
-        }
-
-        return paths;
+        return onBestPath.stream();
     }
 
     private List<Node> getNeighbours(Node current) {
         List<Node> neighbours = newArrayList();
 
-        final Node up = new Node(current.row - 1, current.col, UP, current.distance + 1001, current);
-        final Node down = new Node(current.row + 1, current.col, DOWN, current.distance + 1001, current);
-        final Node left = new Node(current.row, current.col - 1, LEFT, current.distance + 1001, current);
-        final Node right = new Node(current.row, current.col + 1, RIGHT, current.distance + 1001, current);
+        final Node up = new Node(current.row() - 1, current.col(), UP, current.distance() + 1001, current);
+        final Node down = new Node(current.row() + 1, current.col(), DOWN, current.distance() + 1001, current);
+        final Node left = new Node(current.row(), current.col() - 1, LEFT, current.distance() + 1001, current);
+        final Node right = new Node(current.row(), current.col() + 1, RIGHT, current.distance() + 1001, current);
 
-        switch (current.direction) {
+        switch (current.direction()) {
             case UP -> {
-                neighbours.add(new Node(current.row - 1, current.col, UP, current.distance + 1, current));
+                neighbours.add(new Node(current.row() - 1, current.col(), UP, current.distance() + 1, current));
                 neighbours.add(left);
                 neighbours.add(right);
             }
             case DOWN -> {
-                neighbours.add(new Node(current.row + 1, current.col, DOWN, current.distance + 1, current));
+                neighbours.add(new Node(current.row() + 1, current.col(), DOWN, current.distance() + 1, current));
                 neighbours.add(left);
                 neighbours.add(right);
             }
             case LEFT -> {
-                neighbours.add(new Node(current.row, current.col - 1, LEFT, current.distance + 1, current));
+                neighbours.add(new Node(current.row(), current.col() - 1, LEFT, current.distance() + 1, current));
                 neighbours.add(up);
                 neighbours.add(down);
             }
             case RIGHT -> {
-                neighbours.add(new Node(current.row, current.col + 1, RIGHT, current.distance + 1, current));
+                neighbours.add(new Node(current.row(), current.col() + 1, RIGHT, current.distance() + 1, current));
                 neighbours.add(up);
                 neighbours.add(down);
             }
         }
 
-        neighbours.removeIf(node -> !(node.row >= 0 && node.row < map.length && node.col >= 0 && node.col < map[0].length));
-        neighbours.removeIf(node -> map[node.row][node.col] == '#');
+        neighbours.removeIf(node -> !(node.row() >= 0 && node.row() < map.length && node.col() >= 0 && node.col() < map[0].length));
+        neighbours.removeIf(node -> map[node.row()][node.col()] == '#');
         return neighbours;
-    }
-
-    @With
-    record Node(int row, int col, Direction direction, long distance, Node previous) {
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            Node node = (Node) o;
-
-            if (row != node.row) return false;
-            if (col != node.col) return false;
-            return direction == node.direction;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = row;
-            result = 151 * result + col;
-            result = 151 * result + direction.hashCode();
-            return result;
-        }
     }
 }
