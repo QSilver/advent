0167d6b59321032983e0423bc40e78fe976958e3
QSilver <geo_007.popa@yahoo.com>
2025-12-04 21:00:11 +0000

    2025 Day 5 prep

---
+++ src/advent2025/Advent5.java
@@ -0,0 +1,22 @@
+package advent2025;
+
+import lombok.extern.slf4j.Slf4j;
+
+import static util.InputUtils.fileStream;
+
+@Slf4j
+public class Advent5 {
+    // https://adventofcode.com/2025/day/5
+
+    public Long runP1(String file) {
+        fileStream(file);
+
+        return 0L;
+    }
+
+    public Long runP2(String file) {
+        fileStream(file);
+
+        return 0L;
+    }
+}
