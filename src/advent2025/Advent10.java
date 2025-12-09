657f000d07235f90e4f38e674a63c6f91e9f0eeb
QSilver <geo_007.popa@yahoo.com>
2025-12-09 22:19:37 +0000

    2025 Day 10 prep

---
+++ src/advent2025/Advent10.java
@@ -0,0 +1,27 @@
+package advent2025;
+
+import lombok.experimental.ExtensionMethod;
+import lombok.extern.slf4j.Slf4j;
+import util.Extensions;
+
+import java.util.stream.Stream;
+
+import static util.InputUtils.fileStream;
+
+@Slf4j
+@ExtensionMethod({Extensions.class})
+public class Advent10 {
+    // https://adventofcode.com/2025/day/10
+
+    public Long runP1(String file) {
+        Stream<String> stringStream = fileStream(file);
+
+        return 0L;
+    }
+
+    public Long runP2(String file) {
+        Stream<String> stringStream = fileStream(file);
+
+        return 0L;
+    }
+}
---
+++ test/advent2025/Advent10Test.java
@@ -0,0 +1,33 @@
+package advent2025;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Advent10Test {
+    private final Advent10 underTest = new Advent10();
+
+    @Test
+    void testPart1() {
+        Long run = underTest.runP1("advent2025/advent10_ex1.in");
+        assertEquals(50, run);
+    }
+
+    @Test
+    void runPart1() {
+        Long run = underTest.runP1("advent2025/advent10.in");
+        System.out.println(run);
+    }
+
+    @Test
+    void testPart2() {
+        Long run = underTest.runP2("advent2025/advent10_ex1.in");
+        assertEquals(24, run);
+    }
+
+    @Test
+    void runPart2() {
+        Long run = underTest.runP2("advent2025/advent10.in");
+        System.out.println(run);
+    }
+}
